f951a03ac6eb917aa0b5e33a4a6d8d49bb773a41
Ian Sillitoe <ian@sillit.com>
2018-10-13 13:05:15 +0100

    use credentials from test account by default

--- python/tests/api2_test.py
+++ python/tests/api2_test.py
@@ -10,6 +10,9 @@ import requests
 # local convenience
 from . import testutils
 
+TEST_USER='junk@sillit.com'
+TEST_PASSWORD='EGKEyY'
+
 logger = logging.getLogger(__name__)
 
 class TestApi2(testutils.TestCase):
@@ -25,17 +28,10 @@ class TestApi2(testutils.TestCase):
 
         self.base_url = 'https://beta.swissmodel.expasy.org'
 
-        try:
-            self.sm_user = os.environ['SWISSMODEL_USER'] 
-        except:
-            raise Exception('must specify environment variable SWISSMODEL_USER')
+        self.sm_user = os.environ['SWISSMODEL_USER'] if 'SWISSMODEL_USER' in os.environ else TEST_USER
+        self.sm_pass = os.environ['SWISSMODEL_PASSWORD'] if 'SWISSMODEL_PASSWORD' in os.environ else TEST_PASSWORD
 
-        try:
-            self.sm_pass = os.environ['SWISSMODEL_PASSWORD'] 
-        except:
-            raise Exception('must specify environment variable SWISSMODEL_PASSWORD')
-
-        self.expected_project_id = 'jHXF9L'
+        self.incorrect_auth_header = {'Authorization': 'token 0123456789abcdef0123456789abcdef01234567'}
 
         self.request_data = {
             "target_sequence": "KSCCPTTAARNQYNICRLPGTPRPVCAALSGCKIISGTGCPPGYRH",
@@ -53,22 +49,35 @@ class TestApi2(testutils.TestCase):
             "pdb_id": "1crn",
             "label_asym_id": "A",
             "assembly_id": 1,
-            "project_id": self.expected_project_id,
+            "project_id": 'jHXF9L',
         }
 
     def test_submit(self):        
         submit_url = '{}/alignment/'.format(self.base_url)
         r = requests.post(submit_url, data=self.request_data)
-        self.assertEqual(r.status_code, 401, 'should get unauthorised code')
+        self.assertEqual(r.status_code, 401, 'submitting alignment without auth should return unauthorised (401)')
+
+        r = requests.post(submit_url, data=self.request_data, headers=self.incorrect_auth_header)
+        self.assertEqual(r.status_code, 401, 'submitting alignment with made up auth token should return unauthorised (401)')
 
         headers = self.get_auth_headers()
         r = requests.post(submit_url, data=self.request_data, headers=headers)
-        self.assertEqual(r.status_code, 201, 'return status should be 201')
+        self.assertEqual(r.status_code, 201, 'submitting alignment with correct auth token should return 201')
         response_data = r.json()
 
         self.assertDictEqual(response_data, self.expected_response_data)
         project_id = response_data['project_id']
-        self.assertEqual(project_id, self.expected_project_id)
+
+        self.assertRegex(project_id, r'^[a-zA-Z0-9]{6}$')
+
+        status_url = '{}/alignment/{}/status/'.format(self.base_url, project_id)
+        r = requests.get(status_url)
+        self.assertEqual(r.status_code, 401, 'unauthorised request should return 401')
+
+        r = requests.get(status_url, headers=headers)
+        self.assertEqual(r.status_code, 200, 'authorised request should return 200')
+        print( "response [{}]: {}".format(r.status_code, r.json()))
+        response_data = r.json()
 
     def test_auth_headers(self):
         headers = self.get_auth_headers()
@@ -76,19 +85,10 @@ class TestApi2(testutils.TestCase):
         token = headers['Authorization']
         logger.info("auth_token: {}".format(token))
         self.assertRegex(token, r'^token\s+[0-9a-f]{40}')
-        
-    def test_project_status(self):
-        status_url = '{}/alignment/{}/status/'.format(self.base_url, self.expected_project_id)
-        r = requests.get(status_url)
-        self.assertEqual(r.status_code, 401, 'unauthorised request should return 401')
-        headers = { }
-        r = requests.get(status_url, headers)
-        self.assertEqual(r.status_code, 401, 'unauthorised request should return 401')
-        print( "response [{}]: {}".format(r.status_code, r.json()))
-        response_data = r.json()
-
+    
     def get_auth_headers(self):
         data = { 'username': self.sm_user, 'password': self.sm_pass }
+        logger.debug('get_auth_headers.data: {}'.format(data))
         headers = { 'accept': 'application/json' }
         r = requests.post(self.base_url + '/api-token-auth/', data=data, headers=headers)
         logger.debug( "auth_response [{}]: {}".format(r.status_code, r.json()))
