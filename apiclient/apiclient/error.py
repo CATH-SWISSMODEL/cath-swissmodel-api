c7755fd6c83f1390b53bc8bd526bf1afaa130d43
sillitoe <ian@sillit.com>
2019-02-01 20:22:10 +0000

    add some errors

--- apiclient/apiclient/error.py
+++ apiclient/apiclient/error.py
@@ -5,5 +5,11 @@ Defines errors that can occur within this library
 class ApiClientError(Exception):
     pass
 
+class ConfigError(Exception):
+    pass
+
+class ArgError(Exception):
+    pass
+
 class AuthenticationError(ApiClientError):
     pass
