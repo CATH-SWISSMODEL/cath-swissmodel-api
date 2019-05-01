44bcbe65364440e71b5c261f49ae1cfa7ba2c01b
Ian Sillitoe <ian@sillit.com>
2019-05-01 09:18:38 +0100

    improve how response is processed

--- cathsm/cathsm/apiclient/clients.py
+++ cathsm/cathsm/apiclient/clients.py
@@ -4,21 +4,19 @@ Clients that provide interaction with CATH / SWISS-MODEL API.
 
 # core
 import logging
-import sys
-import time
 
 # non-core
 import requests
 
 # local
-from cathsm.apiclient.models import SubmitAlignment
-from cathsm.apiclient.cli import ApiArgumentParser, ApiConfig
-from cathsm.apiclient.errors import AuthenticationError, ArgError
+from cathsm.apiclient.errors import AuthenticationError
 
 DEFAULT_SM_BASE_URL = 'https://beta.swissmodel.expasy.org'
 
-DEFAULT_INFO_FORMAT = '%(asctime)s %(levelname)7s | %(message)s'
-DEFAULT_DEBUG_FORMAT = '%(asctime)s %(name)-30s %(levelname)7s | %(message)s'
+# currently you have to start this yourself...
+# cd ../cathapi && python3 manage.py runserver
+DEFAULT_CATH_BASE_URL = 'http://localhost:8000'
+
 
 LOG = logging.getLogger(__name__)
 
@@ -84,6 +82,18 @@ class SubmitStatusResultsApiClient(ApiClientBase):
 
         return r
 
+    def check_response(self, *, response, status_success=200):
+        if response.status_code != status_success:
+            LOG.error("Error: failed to submit data: status=%s (expected %s), msg=%s",
+                response.status_code, response.text, status_success)
+            response.raise_for_status()
+        try:
+            response_data = response.json()
+        except:
+            LOG.error("failed to get json from response: %s", response)
+            raise
+        return response_data
+
     def submit(self, *, data=None, replacement_fields=None, status_success=201):
         """Submits the alignment data for modelling."""
 
@@ -93,17 +103,9 @@ class SubmitStatusResultsApiClient(ApiClientBase):
             data=data, 
             replacement_fields=replacement_fields)
 
-        self.process_submit_response(r)
+        r = self.process_submit_response(r)
 
-        if r.status_code != status_success:
-            LOG.error("Error: failed to submit data: status={} (expected {}), msg={}".format(
-                r.status_code, r.text, status_success))
-            r.raise_for_status()
-        try:
-            response_data = r.json()
-        except:
-            LOG.error("failed to get json from response: %s", r)
-            raise
+        response_data = self.check_response(response=r, status_success=status_success)
 
         return response_data
 
@@ -120,9 +122,9 @@ class SubmitStatusResultsApiClient(ApiClientBase):
             action='status',
             replacement_fields=replacement_fields)
 
-        self.process_status_response(r)
+        r = self.process_status_response(r)
 
-        response_data = r.json()
+        response_data = self.check_response(response=r, status_success=status_success)
 
         return response_data
 
@@ -139,9 +141,9 @@ class SubmitStatusResultsApiClient(ApiClientBase):
             action='results',
             replacement_fields=replacement_fields)
 
-        self.process_results_response(r)
+        r = self.process_results_response(r)
 
-        response_data = r.json()
+        response_data = self.check_response(response=r, status_success=status_success)
 
         return response_data
 
@@ -174,24 +176,23 @@ class SubmitStatusResultsApiClient(ApiClientBase):
 
     def process_response(self, res):
         """Hook to allow manipulation/inspection of all responses"""
-        pass
+        return res
 
     def process_authenticate_response(self, res):
         """Hook to allow manipulation/inspection of response to `authenticate` action"""
-        pass
+        return res
 
     def process_submit_response(self, res):
         """Hook to allow manipulation/inspection of response to `submit` action"""
-        pass
+        return res
 
     def process_status_response(self, res):
         """Hook to allow manipulation/inspection of response to `status` action"""
-        pass
+        return res
 
     def process_results_response(self, res):
         """Hook to allow manipulation/inspection of response to `results` action"""
-        pass
-
+        return res
 
 class SMAlignmentApiClient(SubmitStatusResultsApiClient):
     """
@@ -237,139 +238,47 @@ class SMAlignmentApiClient(SubmitStatusResultsApiClient):
         """Retrieves the results of an existing project"""
         return super().results(replacement_fields={"project_id": project_id})
 
-class SMAlignmentClient(object):
+
+class CathSelectTemplateApiClient(SubmitStatusResultsApiClient):
     """
-    Generates 3D model from alignment data (via SWISS-MODEL API)
+    Client for API 1 (CATH)
+
+    http://www.cathdb.info/search/by_funfhmmer
+
+    TODO: generate the API paths dynamically via OpenAPI spec(?)
     """
 
-    def __init__(self, *, infile, outfile, api_user=None, api_password=None,
-                 api_token=None, sleep=5, log_level=logging.INFO, config=None,
-                 config_section=None, clear_config=False):
-        
-        if not config_section:
-            config_section=self.__class__.__name__      
+    default_auth_url = '/api/api-token-auth/'
+    default_submit_url = '/api/select-template/'
+    default_status_url = '/api/select-template/{task_id}/'
+    default_results_url = '/api/select-template/{task_id}/results'
 
-        if config is None:
-            config = ApiConfig(section=config_section)
-        else:
-            config.section = config_section
-        
-        if clear_config:
-            LOG.info("Clearing existing config for section: '{}'".format(config.section))
-            config.delete_section()
+    def __init__(self, *,
+                 base_url=DEFAULT_CATH_BASE_URL,
+                 auth_url=default_auth_url,
+                 submit_url=default_submit_url,
+                 status_url=default_status_url,
+                 results_url=default_results_url
+                ):
 
-        self.infile = infile
-        self.outfile = outfile
-        self.sleep = sleep
-        self.log_level = log_level
+        # there has to be a nicer way of doing this?
+        args = {
+            "base_url": base_url,
+            "auth_url": auth_url,
+            "submit_url": submit_url,
+            "status_url": status_url,
+            "results_url": results_url,
+        }
+        super().__init__(**args)
 
-        self.api_token = api_token
-        self.api_user = api_user
-        self.api_password = api_password
+    def submit(self, *, data):
+        """Submits data to the API."""
+        return super().submit(data=data)
 
-        if not self.api_token:
-            if 'api_token' in config:
-                self.api_token = config['api_token']
-            elif self.api_user and self.api_password:
-                pass
-            else:
-                raise ArgError("expected 'api_token' or ('api_user' and 'api_password')")
+    def status(self, task_id):
+        """Retrieves the status of an existing task"""
+        return super().status(replacement_fields={"task_id": task_id})
 
-        self._apiclient = SMAlignmentApiClient()
-        self._config = config
-
-    @classmethod
-    def new_from_cli(cls):
-        parser = ApiArgumentParser(description=cls.__doc__)
-        args = parser.parse_args()
-
-        level=logging.INFO
-        if args.verbose:
-            level=logging.DEBUG
-        if args.quiet:
-            level=logging.WARNING
-        cls.addLogger(level=level)
-
-        passthrough_args = ('infile', 'outfile', 'sleep', 'clear_config',
-            'api_token', 'api_user', 'api_password')
-
-        kwargs = {k: v for k, v in vars(args).items() if k in passthrough_args}
-
-        LOG.debug("Passing CLI args: {}".format(kwargs))
-
-        return cls(**kwargs)
-
-    @classmethod
-    def addLogger(cls, *, level=logging.INFO, logger=None, handler=None, formatter=None):
-
-        if not formatter:
-            if level <= logging.DEBUG:
-                log_format = DEFAULT_DEBUG_FORMAT 
-            elif level >= logging.INFO:
-                log_format = DEFAULT_INFO_FORMAT
-            formatter = logging.Formatter(log_format)
-
-        if not handler:
-            handler = logging.StreamHandler()
-
-        if not logger:
-            logger = logging.getLogger()
-
-        logger.setLevel(level)
-        handler.setFormatter(formatter)
-        logger.addHandler(handler)
-
-    def run(self):
-    
-        api = self._apiclient
-        config = self._config
-
-        LOG.info("DATA:  {}".format(self.infile))
-        LOG.info("MODEL: {}".format(self.outfile))
-
-        if self.api_token is not None:
-            LOG.info("Setting API token ... ")
-            api.set_token(api_token=self.api_token)
-        else:
-            LOG.info("Authenticating via user/pass... (token_id not provided) ")
-            api_token = api.authenticate(api_user=self.api_user, api_pass=self.api_password)
-            LOG.debug("Saving API token to config file")
-            config['api_token'] = api_token
-
-        LOG.info("Loading data from file '%s' ...", self.infile)
-        with open(self.infile) as infile:
-            submit_data = SubmitAlignment.load(infile)
-
-        LOG.info("Submitting data ... ")
-        submit_r = api.submit(data=submit_data)
-        project_id = submit_r['project_id']
-        
-        LOG.info("Checking status of project <{}> ...".format(project_id))
-        while True:
-            status_r = api.status(project_id)
-            status = status_r['status']
-            LOG.info("   status: %s", status)
-            if status == 'COMPLETED':
-                break
-            if status == 'FAILED':
-                LOG.error("Modelling failed: " + status_r['message'])
-                sys.exit(1)
-            else:
-                time.sleep(self.sleep)
-
-        LOG.info("Retrieving results ... ")
-        result_r = api.results(project_id)
-        
-        def truncate_strings(s):
-            if type(s) == str and len(s) > 100:
-                s = str(s[:100] + '...')
-            return s
-
-        truncated_result = {k: truncate_strings(v) for k, v in result_r.items()}
-        LOG.debug("result: {}".format(truncated_result))
-        coords = result_r['coordinates']
-        
-        LOG.info("Writing coordinates to {}".format(self.outfile))
-        with open(self.outfile, 'w') as outfile:
-            outfile.write(coords)
-
+    def results(self, task_id):
+        """Retrieves the results of an existing task"""
+        return super().results(replacement_fields={"task_id": task_id})
