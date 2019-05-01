f94f6367018cc8bdf52e8f793924f7ed9a25d51f
Ian Sillitoe <ian@sillit.com>
2019-05-01 09:21:58 +0100

    update tests

---
+++ cathsm/tests/api1_test.py
@@ -0,0 +1,58 @@
+# core
+import json
+import os
+import configparser
+import tempfile
+import unittest
+
+# local
+from cathsm.apiclient.config import ApiConfig
+from cathsm.apiclient.clients import CathSelectTemplateApiClient
+from cathsm.apiclient.managers import CathSelectTemplateApiClientManager
+
+DELETE_TEMP_FILES=False
+
+EXAMPLE_DATA_PATH = os.path.join(os.path.dirname(__file__), '..', 'example_data')
+EXAMPLE_USER='junk@sillit.com'
+EXAMPLE_PASSWORD='FJRbnz'
+
+class CathSelectTemplateApiClientTest(unittest.TestCase):
+
+    def test_defaults(self):
+        client = CathSelectTemplateApiClient()
+        self.assertIsInstance(client, CathSelectTemplateApiClient)
+                
+        self.assertEqual(
+            client.get_url('submit'),
+            base_url + '/select-template/')
+        
+        self.assertEqual(
+            client.get_url('status', {"task_id": "foo"}),
+            base_url + '/select-template/foo/')
+        
+        self.assertEqual(
+            client.get_url('results', {"task_id": "foo"}),
+            base_url + '/select-template/foo/results')
+
+
+class SMAlignmentClientTest(unittest.TestCase):
+
+    def test_defaults(self):
+
+        infile = os.path.join(EXAMPLE_DATA_PATH, 'select-template-01.json')
+
+        # run with user auth
+        outfile1 = tempfile.NamedTemporaryFile(delete=DELETE_TEMP_FILES)
+        client1 = CathSelectTemplateClient(infile=infile, outfile=outfile1.name, 
+            api_user=EXAMPLE_USER, api_password=EXAMPLE_PASSWORD)
+        client1.run()
+
+        api_token = client1.api_token
+
+        # run with token auth
+        outfile2 = tempfile.NamedTemporaryFile(delete=DELETE_TEMP_FILES)
+        client2 = CathSelectTemplateClient(infile=infile, outfile=outfile2.name, 
+            api_token=api_token)
+        client2.run()
+        self.assertTrue(os.path.isfile(outfile2.name), 'outfile exists (token auth)')
+
--- cathsm/tests/api2_test.py
+++ cathsm/tests/api2_test.py
@@ -7,7 +7,8 @@ import unittest
 
 # local
 from cathsm.apiclient.config import ApiConfig
-from cathsm.apiclient.clients import SMAlignmentApiClient, SMAlignmentClient
+from cathsm.apiclient.clients import SMAlignmentApiClient
+from cathsm.apiclient.managers import SMAlignmentApiClientManager
 
 DELETE_TEMP_FILES=False
 
@@ -35,56 +36,6 @@ class SMAlignmentApiClientTest(unittest.TestCase):
         self.assertEqual(
             client.get_url('results', {"project_id": "foo"}),
             base_url + '/alignment/foo/')
-
-class ApiConfigTest(unittest.TestCase):
-
-    def test_defaults(self):
-
-        section1 = 'api1'
-        section2 = 'api2'
-
-        tmp_file = tempfile.NamedTemporaryFile(delete=DELETE_TEMP_FILES)
-        config_fname = tmp_file.name
-        tmp_file.close()
-
-        def parse_config():
-            conf = configparser.ConfigParser()
-            conf.read(config_fname)
-            d = {}
-            for s in conf.sections():
-                d[s] = dict(conf[s])
-            return d
-
-        config1 = ApiConfig(section=section1, filename=config_fname)
-        
-        self.assertIsNot(os.path.isfile(config_fname), 
-            'config file does not exist at start')
-
-        config1['foo'] = 'bar'
-        self.assertDictEqual(parse_config(), {"api1": {"foo": "bar"} }, 
-            'saved config data contains prefix')
-        self.assertEqual(config1['foo'], 'bar',
-            'prefix works as expected when retrieving data')
-
-        config2 = ApiConfig(section=section2, filename=config_fname)
-        config2['foo'] = 'barf'
-
-        self.assertDictEqual(dict(parse_config()), {"api1": {"foo": "bar"}, "api2": {"foo": "barf"}}, 
-            'same key different project looks okay')
-
-        self.assertEqual(config2['foo'], 'barf',
-            'prefix works as expected when retrieving data')
-
-        config2['foo'] = 'bingo'
-        config2['food'] = 'apple'
-
-        self.assertDictEqual(parse_config(), {"api1": {"foo": "bar"}, "api2": {"foo": "bingo", "food": "apple"}},
-            'add/updated data looks okay')
-
-        config2.delete_section(section_id=section2)
-        self.assertDictEqual(parse_config(), {"api1": {"foo": "bar"}}, 
-            'delete_section() works okay')
-
 
 class SMAlignmentClientTest(unittest.TestCase):
 
