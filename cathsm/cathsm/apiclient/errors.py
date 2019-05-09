70648ccdf551f5e8b8d492b4821eedd08f68f6eb
Ian Sillitoe <ian@sillit.com>
2019-05-09 23:25:55 +0100

    add NoResultsError

--- cathsm/cathsm/apiclient/errors.py
+++ cathsm/cathsm/apiclient/errors.py
@@ -2,14 +2,22 @@
 Defines errors that can occur within this library
 """
 
+
 class ApiClientError(Exception):
     pass
+
 
 class ConfigError(Exception):
     pass
 
+
 class ArgError(Exception):
     pass
 
+
 class AuthenticationError(ApiClientError):
+    pass
+
+
+class NoResultsError(Exception):
     pass
