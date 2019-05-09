2eb18fcb98bf921a7e3627b9c758d179fefbebb3
Ian Sillitoe <ian@sillit.com>
2019-05-09 23:26:35 +0100

    override config.token if user/pass is supplied

--- cathsm/cathsm/apiclient/managers.py
+++ cathsm/cathsm/apiclient/managers.py
@@ -22,19 +22,19 @@ class ApiClientManagerBase(object):
 
     def __init__(self, *, infile, outfile, api_client,
                  sleep=5, log_level=logging.INFO,
-                 api_user=None, api_password=None, api_token=None, 
+                 api_user=None, api_password=None, api_token=None,
                  config=None, config_section=None, clear_config=False):
-        
+
         if not config_section:
-            config_section=self.__class__.__name__      
+            config_section = self.__class__.__name__
 
         if config is None:
             config = ApiConfig(section=config_section)
         else:
             config.section = config_section
-        
+
         if clear_config:
-            LOG.info("Clearing existing config for section: '{}'".format(config.section))
+            LOG.info("Clearing existing config for section: '%s'", config.section)
             config.delete_section()
 
         self.infile = infile
@@ -49,12 +49,13 @@ class ApiClientManagerBase(object):
         self.api_client = api_client
 
         if not self.api_token:
-            if 'api_token' in config:
-                self.api_token = config['api_token']
-            elif self.api_user and self.api_password:
+            if self.api_user and self.api_password:
                 pass
+            elif 'api_token' in config:
+                self.api_token = config['api_token']
             else:
-                raise ArgError("expected 'api_token' or ('api_user' and 'api_password')")
+                raise ArgError(
+                    "expected 'api_token' or ('api_user', 'api_password')")
 
         self._config = config
 
@@ -64,19 +65,19 @@ class ApiClientManagerBase(object):
             parser = ApiArgumentParser(description=cls.__doc__)
         args = parser.parse_args()
 
-        level=logging.INFO
+        level = logging.INFO
         if args.verbose:
-            level=logging.DEBUG
+            level = logging.DEBUG
         if args.quiet:
-            level=logging.WARNING
+            level = logging.WARNING
         cls.addLogger(level=level)
 
         passthrough_args = ('infile', 'outfile', 'sleep', 'clear_config',
-            'api_token', 'api_user', 'api_password')
+                            'api_token', 'api_user', 'api_password')
 
         kwargs = {k: v for k, v in vars(args).items() if k in passthrough_args}
 
-        LOG.debug("Passing CLI args: {}".format(kwargs))
+        LOG.debug("Passing CLI args: %s", kwargs)
 
         return cls(**kwargs)
 
@@ -85,7 +86,7 @@ class ApiClientManagerBase(object):
 
         if not formatter:
             if level <= logging.DEBUG:
-                log_format = DEFAULT_DEBUG_FORMAT 
+                log_format = DEFAULT_DEBUG_FORMAT
             elif level >= logging.INFO:
                 log_format = DEFAULT_INFO_FORMAT
             formatter = logging.Formatter(log_format)
@@ -107,45 +108,62 @@ class ApiClientManagerBase(object):
 
         if self.api_token is not None:
             LOG.info("Setting API token ... ")
-            api.set_token(api_token=self.api_token)
+            api_token = self.api_token
         else:
             LOG.info("Authenticating via user/pass... (token_id not provided) ")
-            api_token = api.authenticate(api_user=self.api_user, api_pass=self.api_password)
-            LOG.debug("Saving API token to config file")
-            config['api_token'] = api_token
+            LOG.debug("user: %s", self.api_user)
+            api_token = api.authenticate(
+                api_user=self.api_user, api_pass=self.api_password)
+
+        LOG.debug("Saving API token to config file")
+        config['api_token'] = api_token
+
+        LOG.debug("Setting API token in client")
+        api.set_token(api_token=api_token)
 
 
 class CathSelectTemplateManager(ApiClientManagerBase):
     """
     Selected template structure from sequence (via CATH API)
+
+    Args:
+        api_client (CathSelectTemplateClient): override default client (optional)
+        base_url (str): create the default client with this base URL (optional)
     """
 
-    def __init__(self, *args, api_client=None, **kwargs):
+    def __init__(self, *, api_client=None, base_url=None, **kwargs):
         self._submit_data_cls = SubmitSelectTemplate
+        client_args = {}
+        if base_url:
+            client_args['base_url'] = base_url
         if not api_client:
-            api_client = clients.CathSelectTemplateClient()
+            api_client = clients.CathSelectTemplateClient(**client_args)
+
+        self.results_json = None
         super().__init__(api_client=api_client, **kwargs)
 
     def run(self):
-    
+
         api = self.api_client
         config = self._config
 
-        LOG.info("IN_FILE:  {}".format(self.infile))
-        LOG.info("OUT_FILE: {}".format(self.outfile))
+        LOG.info("IN_FILE:  %s", self.infile)
+        LOG.info("OUT_FILE: %s", self.outfile)
 
+        LOG.info("Authenticating...")
         self.authenticate()
+        LOG.info("  ... got token: %s", self.api_token)
 
         LOG.info("Loading data from file '%s' ...", self.infile)
         with open(self.infile) as infile:
             submit_data = SubmitSelectTemplate.load(infile)
 
-        LOG.info("Submitting data ... ")
+        LOG.info("Submitting data ... %s", str(submit_data.__dict__))
         submit_r = api.submit(data=submit_data)
-        LOG.info("Response: %s", submit_r)
+        LOG.debug("response: %s", submit_r)
         task_id = submit_r['uuid']
-        
-        LOG.info("Checking status of task <{}> ...".format(task_id))
+
+        LOG.info("Checking status of task <%s> ...", task_id)
         while True:
             status_r = api.status(task_id)
             status = status_r['status'].upper()
@@ -160,30 +178,41 @@ class CathSelectTemplateManager(ApiClientManagerBase):
 
         LOG.info("Retrieving results ... ")
         result_r = api.results(task_id)
-        
-        LOG.info("result: {}".format(result_r))
-        
-        LOG.info("Writing resolved hits to {}".format(self.outfile))
-        with open(self.outfile, 'w') as outfile:
-            outfile.write()
+
+        LOG.debug("result: %s", str(result_r)[:100])
+
+        self.results_json = result_r
+
+        LOG.info("Writing resolved hits to %s", self.outfile)
+        # with open(self.outfile, 'w') as outfile:
+        #     outfile.write()
+
+    def funfam_scan(self):
+        """Returns the funfam scan results as :class:`cathpy.models.Scan`"""
+        return self.api_client.funfam_scan()
+
+    def funfam_resolved_scan(self):
+        """Returns the resolved funfam scan results as :class:`cathpy.models.Scan`"""
+        return self.api_client.funfam_resolved_scan()
+
 
 class SMAlignmentManager(ApiClientManagerBase):
     """
     Generates 3D model from alignment data (via SWISS-MODEL API)
     """
 
-    def __init__(self, *args, api_client=None, **kwargs):
+    def __init__(self, *, api_client=None, **kwargs):
         if not api_client:
             api_client = clients.SMAlignmentClient()
         super().__init__(api_client=api_client, **kwargs)
 
     def run(self):
-    
+
         api = self.api_client
         config = self._config
 
-        LOG.info("IN_FILE:  {}".format(self.infile))
-        LOG.info("OUT_FILE: {}".format(self.outfile))
+        LOG.info("IN_FILE:  %s", self.infile)
+        LOG.info("OUT_FILE: %s", self.outfile)
 
         self.authenticate()
 
@@ -194,8 +223,8 @@ class SMAlignmentManager(ApiClientManagerBase):
         LOG.info("Submitting data ... ")
         submit_r = api.submit(data=submit_data)
         project_id = submit_r['project_id']
-        
-        LOG.info("Checking status of project <{}> ...".format(project_id))
+
+        LOG.info("Checking status of project <%s> ...", project_id)
         while True:
             status_r = api.status(project_id)
             status = status_r['status']
@@ -203,24 +232,24 @@ class SMAlignmentManager(ApiClientManagerBase):
             if status == 'COMPLETED':
                 break
             if status == 'FAILED':
-                LOG.error("Modelling failed: " + status_r['message'])
+                LOG.error("Modelling failed: %s", status_r['message'])
                 sys.exit(1)
             else:
                 time.sleep(self.sleep)
 
         LOG.info("Retrieving results ... ")
         result_r = api.results(project_id)
-        
-        def truncate_strings(s):
-            if type(s) == str and len(s) > 100:
-                s = str(s[:100] + '...')
-            return s
 
-        truncated_result = {k: truncate_strings(v) for k, v in result_r.items()}
-        LOG.debug("result: {}".format(truncated_result))
+        def truncate_string(in_str):
+            if isinstance(in_str, str) and len(in_str) > 100:
+                in_str = str(in_str[:100] + '...')
+            return in_str
+
+        truncated_result = {k: truncate_string(
+            v) for k, v in result_r.items()}
+        LOG.debug("result: %s", truncated_result)
         coords = result_r['coordinates']
-        
-        LOG.info("Writing coordinates to {}".format(self.outfile))
+
+        LOG.info("Writing coordinates to %s", self.outfile)
         with open(self.outfile, 'w') as outfile:
             outfile.write(coords)
-
