94faf5f9038dba57b5f37537ae2a5208766b3edd
Ian Sillitoe <ian@sillit.com>
2019-05-09 23:27:41 +0100

    lint changes

--- cathsm/tests/api2_test.py
+++ cathsm/tests/api2_test.py
@@ -6,32 +6,35 @@ import unittest
 # local
 from cathsm.apiclient import clients, managers
 
-DELETE_TEMP_FILES=False
+DELETE_TEMP_FILES = False
 
-EXAMPLE_DATA_PATH = os.path.join(os.path.dirname(__file__), '..', 'example_data')
-EXAMPLE_USER='junk@sillit.com'
-EXAMPLE_PASSWORD='FJRbnz'
+EXAMPLE_DATA_PATH = os.path.join(
+    os.path.dirname(__file__), '..', 'example_data')
+EXAMPLE_USER = 'junk@sillit.com'
+EXAMPLE_PASSWORD = 'FJRbnz'
+
 
 class SMAlignmentClientTest(unittest.TestCase):
 
     def test_defaults(self):
         client = clients.SMAlignmentClient()
         self.assertIsInstance(client, clients.SMAlignmentClient)
-        
+
         base_url = client.base_url
         self.assertEqual(base_url, 'https://beta.swissmodel.expasy.org')
-        
+
         self.assertEqual(
             client.get_url('submit'),
             base_url + '/alignment/')
-        
+
         self.assertEqual(
             client.get_url('status', {"project_id": "foo"}),
             base_url + '/alignment/foo/status/')
-        
+
         self.assertEqual(
             client.get_url('results', {"project_id": "foo"}),
             base_url + '/alignment/foo/')
+
 
 class SMAlignmentManagerTest(unittest.TestCase):
 
@@ -47,20 +50,23 @@ class SMAlignmentManagerTest(unittest.TestCase):
 
         # run with user auth
         outfile1 = tempfile.NamedTemporaryFile(delete=DELETE_TEMP_FILES)
-        client1 = managers.SMAlignmentManager(infile=infile, outfile=outfile1.name, 
-            api_user=EXAMPLE_USER, api_password=EXAMPLE_PASSWORD)
+        client1 = managers.SMAlignmentManager(infile=infile, outfile=outfile1.name,
+                                              api_user=EXAMPLE_USER, api_password=EXAMPLE_PASSWORD)
         client1.run()
 
         api_token = client1.api_token
 
-        self.assertTrue(os.path.isfile(outfile1.name), 'outfile exists (user auth)')
-        self.assertEqual(len(atom_lines(outfile1.name)), 2187, 'correct number of atoms')
+        self.assertTrue(os.path.isfile(outfile1.name),
+                        'outfile exists (user auth)')
+        self.assertEqual(len(atom_lines(outfile1.name)),
+                         2187, 'correct number of atoms')
 
         # run with token auth
         outfile2 = tempfile.NamedTemporaryFile(delete=DELETE_TEMP_FILES)
-        client2 = managers.SMAlignmentManager(infile=infile, outfile=outfile2.name, 
-            api_token=api_token)
+        client2 = managers.SMAlignmentManager(infile=infile, outfile=outfile2.name,
+                                              api_token=api_token)
         client2.run()
-        self.assertTrue(os.path.isfile(outfile2.name), 'outfile exists (token auth)')
-        self.assertEqual(len(atom_lines(outfile2.name)), 2187, 'correct number of atoms')
-
+        self.assertTrue(os.path.isfile(outfile2.name),
+                        'outfile exists (token auth)')
+        self.assertEqual(len(atom_lines(outfile2.name)),
+                         2187, 'correct number of atoms')
