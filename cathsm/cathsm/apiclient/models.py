eae63e6248e81ee9f21896bfc4dc52eff6d737bd
Ian Sillitoe <ian@sillit.com>
2019-05-09 23:26:48 +0100

    lint changes

--- cathsm/cathsm/apiclient/models.py
+++ cathsm/cathsm/apiclient/models.py
@@ -3,7 +3,11 @@ import logging
 
 LOG = logging.getLogger(__name__)
 
+
 class Model(object):
+    """
+    Base class for all models
+    """
 
     @classmethod
     def load(cls, infile):
@@ -11,7 +15,8 @@ class Model(object):
         try:
             data = json.load(infile)
         except Exception as err:
-            raise Exception("failed to load {} from json file '{}': {}".format(cls, infile, err))
+            raise Exception(
+                "failed to load {} from json file '{}': {}".format(cls, infile, err))
         return cls(**data)
 
     def as_dict(self, *, remove_meta=True):
@@ -19,7 +24,7 @@ class Model(object):
         data = self.__dict__
         if remove_meta and 'meta' in data:
             del data['meta']
-        data = dict((k, v) for k, v in data.items() if v != None)
+        data = dict((k, v) for k, v in data.items() if v is not None)
         return data
 
 
